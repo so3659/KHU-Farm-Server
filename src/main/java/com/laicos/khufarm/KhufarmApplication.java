d758b17633139090cd79413493fc20c8838d6c96
so3659 <so3659@naver.com>
2025-06-05 22:41:08 +0900

    🎉 Project Setting
    프로젝트 초기 세팅

---
+++ src/main/java/com/laicos/khufarm/KhufarmApplication.java
@@ -0,0 +1,13 @@
+package com.laicos.khufarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhufarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhufarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laicos/khufarm/KhufarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laicos.khufarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhufarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
