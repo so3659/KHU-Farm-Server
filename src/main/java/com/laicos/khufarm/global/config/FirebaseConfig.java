ae48cea1e9bebdd81d8ff72649873ddf08d9acf5
so3659 <so3659@naver.com>
2025-07-28 01:43:21 +0900

    ♻️ [REFAC] json위치 수정

--- src/main/java/com/laicos/khufarm/global/config/FirebaseConfig.java
+++ src/main/java/com/laicos/khufarm/global/config/FirebaseConfig.java
@@ -5,22 +5,30 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
     @PostConstruct
     public void init(){
-        try (FileInputStream serviceAccount = new FileInputStream("./src/main/resources/firebase/khu-farm-firebase-adminsdk-fbsvc-88f1c036b2.json")) {
+        try {
+            // ClassPathResource를 사용하여 클래스패스에서 리소스를 읽어옴
+            String firebaseKeyPath = "firebase/khu-farm-firebase-adminsdk-fbsvc-88f1c036b2.json";
+            ClassPathResource resource = new ClassPathResource(firebaseKeyPath);
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+            // InputStream을 얻음
+            try (InputStream serviceAccount = resource.getInputStream()) {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
 
-            if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 이미 초기화되어 있지 않은 경우에만 초기화 실행
-                FirebaseApp.initializeApp(options);
+                if (FirebaseApp.getApps().isEmpty()) {
+                    FirebaseApp.initializeApp(options);
+                }
             }
         } catch (IOException e){
             e.printStackTrace();
