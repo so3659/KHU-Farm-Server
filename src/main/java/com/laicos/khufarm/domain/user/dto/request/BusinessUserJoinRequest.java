8f3e9c7bfa9a088cfeb861b047f87c7a4b054942
so3659 <so3659@naver.com>
2025-08-13 00:08:08 +0900

    ♻️ [REFAC] Request 전화번호 삭제

--- src/main/java/com/laicos/khufarm/domain/user/dto/request/BusinessUserJoinRequest.java
+++ src/main/java/com/laicos/khufarm/domain/user/dto/request/BusinessUserJoinRequest.java
@@ -26,10 +26,6 @@ public class BusinessUserJoinRequest {
     @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "올바른 아이디 형식이 아닙니다")
     private String userId;
 
-    @NotNull(message = "휴대폰 번호는 필수 입력값입니다.")
-    @Pattern(regexp = "^\\d{2,3}\\d{3,4}\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
-    private String phoneNumber;
-
     @NotNull(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
